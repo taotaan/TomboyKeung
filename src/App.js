988028e5c5a147207448de926e9b18d6b8444312
taotaan <pariidd09@gmail.com>
2025-06-14 16:14:48 +0700

    tom commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-
+import Layout from './components/Layout';
 import Products from './pages/Products';
 import Exchange from './pages/Exchange';
 import Donate from './pages/Donate';
@@ -12,12 +12,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/exchange" element={<Exchange />} />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="/sell" element={<Sell />} />
-        {/* สามารถเพิ่ม Route อื่นๆ ได้ที่นี่ */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="exchange" element={<Exchange />} />
+          <Route path="donate" element={<Donate />} />
+          <Route path="sell" element={<Sell />} />
+        </Route>
       </Routes>
     </Router>
   );
