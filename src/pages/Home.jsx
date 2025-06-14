9ce9ad57e8399f869b547523272cc3e15dffdba5
taotaan <pariidd09@gmail.com>
2025-06-14 10:35:22 +0700

    two commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,8 +25,8 @@ function Home() {
   <h1>♻️ เปลี่ยนเสื้อผ้าเก่าให้มีค่า</h1>
   <p>ลดขยะ สร้างรายได้ ร่วมสร้างเมืองใหม่สุรนารีให้น่าอยู่</p>
   <div className="btns">
-    <button className="sell">ลงขายสินค้า</button>
-    <button className="sell">แลกเปลี่ยนเสื้อผ้า</button>
+  <Link to="/products" className="sell">ลงขายสินค้า</Link>
+    <Link to="/exchange" className="sell">แลกเปลี่ยนเสื้อผ้า</Link>
   </div>
 </div>
 
